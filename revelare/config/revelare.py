e7caae784cec0de83c643ed36f0e293aef3ca13a
monroy95 <m.monroyc22@gmail.com>
2018-03-06 11:46:21 -0600

    Agregando Configuracion en app para mostrar acceso a page

---
+++ revelare/config/revelare.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Setup"),
+			"icon": "fa fa-cog",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Configuration",
+					"description": _("General Configuration to Revelare.")
+				},
+			]
+		},
+		{
+			"label": _("Analytics"),
+			"icon": "fa fa-table",
+			"items": [
+				{
+					"type": "page",
+					"name": "sales-analytics-2",
+					"label": _("Sales Analytics 2"),
+					"icon": "fa fa-bar-chart",
+				},
+			]
+		},
+	]
