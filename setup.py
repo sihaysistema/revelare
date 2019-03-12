510e3f1a8da11fa8246c010866b2cf80eab156b3
Mario Monroy <m.monroyc22@gmail.com>
2019-03-11 21:39:35 -0600

    Update setup.py +pip10

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+# from pip.req import parse_requirements
 import re, ast
+
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in revelare/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
@@ -10,7 +13,7 @@ with open('revelare/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
+# requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='revelare',
@@ -22,5 +25,5 @@ setup(
 	zip_safe=False,
 	include_package_data=True,
 	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	# dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
