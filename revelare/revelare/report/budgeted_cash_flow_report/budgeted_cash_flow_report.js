e2f23a873ba37e85037d1ed7b9157112501a6676
monroy95 <m.monroyc22@gmail.com>
2019-03-12 23:18:38 -0600

    WIP generar reporte

--- revelare/revelare/report/budgeted_cash_flow_report/budgeted_cash_flow_report.js
+++ revelare/revelare/report/budgeted_cash_flow_report/budgeted_cash_flow_report.js
@@ -1,9 +1,47 @@
 // Copyright (c) 2016, SHS and contributors
 // For license information, please see license.txt
 /* eslint-disable */
+frappe.require("assets/erpnext/js/financial_statements.js");
+
 
 frappe.query_reports["Budgeted Cash Flow Report"] = {
 	"filters": [
-
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"reqd": 1
+		},
+		{
+			"fieldname": "from_fiscal_year",
+			"label": __("Start Year"),
+			"fieldtype": "Link",
+			"options": "Fiscal Year",
+			"default": frappe.defaults.get_user_default("fiscal_year"),
+			"reqd": 1
+		},
+		{
+			"fieldname": "to_fiscal_year",
+			"label": __("End Year"),
+			"fieldtype": "Link",
+			"options": "Fiscal Year",
+			"default": frappe.defaults.get_user_default("fiscal_year"),
+			"reqd": 1
+		},
+		{
+			"fieldname": "periodicity",
+			"label": __("Periodicity"),
+			"fieldtype": "Select",
+			"options": [
+				{ "value": "Monthly", "label": __("Monthly") },
+				{ "value": "Quarterly", "label": __("Quarterly") },
+				{ "value": "Half-Yearly", "label": __("Half-Yearly") },
+				{ "value": "Yearly", "label": __("Yearly") }
+			],
+			"default": "Monthly",
+			"reqd": 1
+		}
 	]
 }
