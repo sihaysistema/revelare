ac5434f35912e50fd31339565ccaca02eacdd437
mmonroy <m.monroy22@gmail.com>
2019-04-01 22:03:56 -0600

    Mostrando Data

--- revelare/revelare/report/budgeted_cash_flow_report/budgeted_cash_flow_report.py
+++ revelare/revelare/report/budgeted_cash_flow_report/budgeted_cash_flow_report.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 from frappe.utils import flt, cint
-from erpnext.accounts.report.financial_statements import (get_period_list, get_columns, get_data)
+from erpnext.accounts.report.financial_statements import (get_period_list, get_data)
 
 def execute(filters=None):
 	# columns, data = [{
@@ -24,47 +24,65 @@ def execute(filters=None):
 
 	columns = get_columns(filters.periodicity, period_list, filters.accumulated_values, company=filters.company)
 	data = [{
-		"year_end_date": "2019-12-31",
-		"parent_account": "",
-		"dec_2019": 39.0,
-		"account_type": "",
-		"year_start_date": "2019-01-01",
-		"has_value": True,
-		"is_group": 1,
-		"account": "1000 - UTILIZACI\xd3N DE FONDOS (ACTIVOS) - S",
+		"total": 0,
 		"currency": "GTQ",
-		"total": "",
-		"indent": 0.0,
-		"include_in_gross": 0,
-		"account_name": "1000 - UTILIZACI\xd3N DE FONDOS (ACTIVOS)",
-		"opening_balance": 0.0
+		"party_cash_flow": "Saldo Inicial",
+		# "dec_2019": 39.0,
 	},
 	{
-		"year_end_date": "2019-12-31",
-		"parent_account": "1300 - Cuentas por cobrar - S",
-		"dec_2019": 39.0,
-		"account_type": "Receivable",
-		"year_start_date": "2019-01-01",
-		"has_value": True,
-		"is_group": 0,
-		"account": "1310 - DEUDORES VARIOS - S",
 		"currency": "GTQ",
-		"total": 39.0,
-		"indent": 2.0,
-		"include_in_gross": 0,
-		"account_name": "1310 - DEUDORES VARIOS",
-		"opening_balance": 0.0
-	},
-	{
-		"total": 39.0,
-		"currency": "GTQ",
-		"account": "Total Asset (Debit)",
-		"dec_2019": 39.0,
-		"account_name": "Total Asset (Debit)"
-	}]
+		"party_cash_flow": "INGRESOS",
+	}
+	# {
+	# 	"year_end_date": "2019-12-31",
+	# 	"parent_account": "",
+	# 	"dec_2019": 39.0,
+	# 	"account_type": "",
+	# 	"year_start_date": "2019-01-01",
+	# 	"has_value": True,
+	# 	"is_group": 1,
+	# 	"party_cash_flow": "1000 - UTILIZACI\xd3N DE FONDOS (ACTIVOS) - S",
+	# 	"currency": "GTQ",
+	# 	"total": "",
+	# 	"indent": 0.0,
+	# 	"include_in_gross": 0,
+	# 	"account_name": "1000 - UTILIZACI\xd3N DE FONDOS (ACTIVOS)",
+	# 	"opening_balance": 0.0
+	# },
+	# {
+	# 	"year_end_date": "2019-12-31",
+	# 	"parent_account": "1300 - Cuentas por cobrar - S",
+	# 	"dec_2019": 39.0,
+	# 	"account_type": "Receivable",
+	# 	"year_start_date": "2019-01-01",
+	# 	"has_value": True,
+	# 	"is_group": 0,
+	# 	"party_cash_flow": "1310 - DEUDORES VARIOS - S",
+	# 	"currency": "GTQ",
+	# 	"total": 39.0,
+	# 	"indent": 1.0,
+	# 	"include_in_gross": 0,
+	# 	"account_name": "1310 - DEUDORES VARIOS",
+	# 	"opening_balance": 0.0
+	# },
+	# {
+	# 	"total": 39.0,
+	# 	"currency": "GTQ",
+	# 	"party_cash_flow": "Total Ingresos",
+	# 	"dec_2019": 39.0
+	# }
+	]
 
 	# chart = get_chart_data(filters, columns, asset, liability, equity)
 	# frappe.msgprint(_(str(columns)))
+	# frappe.msgprint(_(str(get_data_cash_flow(filters.company))))
+
+	datos_registro = get_data_cash_flow(filters.company)
+
+	# frappe.msgprint(_(str(prepare_data(datos_registro, period_list, 'GTQ'))))
+	data.extend(prepare_data(datos_registro, period_list, 'GTQ'))
+
+	# frappe.msgprint(_(str(data)))
 	return columns, data
 
 
@@ -102,3 +120,86 @@ def get_chart_data(filters, columns, asset, liability, equity):
 		chart["type"] = "line"
 
 	return chart
+
+
+def get_columns(periodicity, period_list, accumulated_values=1, company=None):
+	columns = [{
+		"fieldname": "party_cash_flow",
+		"label": _("Party Cash Flow"),
+		"fieldtype": "Link",
+		"options": "Budgeted Cash Flow",
+		"width": 300
+	}]
+	if company:
+		columns.append({
+			"fieldname": "currency",
+			"label": _("Currency"),
+			"fieldtype": "Link",
+			"options": "Currency",
+			"hidden": 1
+		})
+	for period in period_list:
+		columns.append({
+			"fieldname": period.key,
+			"label": period.label,
+			"fieldtype": "Currency",
+			"options": "currency",
+			"width": 150
+		})
+	if periodicity!="Yearly":
+		if not accumulated_values:
+			columns.append({
+				"fieldname": "total",
+				"label": _("Total"),
+				"fieldtype": "Currency",
+				"width": 150
+			})
+
+	return columns
+
+
+def prepare_data(reg_chash_flow, period_list, company_currency):
+	data = []
+	year_start_date = period_list[0]["year_start_date"].strftime("%Y-%m-%d")
+	year_end_date = period_list[-1]["year_end_date"].strftime("%Y-%m-%d")
+
+	for d in reg_chash_flow:
+		# add to output
+		has_value = False
+		total = 0
+		row = frappe._dict({
+			"party_cash_flow": _(d.party),
+			"indent": flt(0),
+			"year_start_date": year_start_date,
+			"year_end_date": year_end_date,
+			"currency": company_currency,
+			"is_group": 0,
+			# "opening_balance": d.get("opening_balance", 0.0) * (1 if balance_must_be=="Debit" else -1),
+			"total": d.paid_amount,
+		})
+		for period in period_list:
+			if d.get(period.key):
+				# change sign based on Debit or Credit, since calculation is done using (debit - credit)
+				d[period.key] *= +1
+
+			row[period.key] = flt(d.get(period.key, 0.0), 3)
+
+			if abs(row[period.key]) >= 0.005:
+				# ignore zero values
+				has_value = True
+				total += flt(row[period.key])
+
+		row["has_value"] = has_value
+		row["total"] = total
+		data.append(row)
+
+	return data
+
+
+def get_data_cash_flow(company):
+	data_cash_flow = frappe.db.get_values('Budgeted Cash Flow',
+										filters={'company': company, 'status_payment': 'Unpaid'},
+										fieldname=['name', 'party', 'paid_amount'],
+										as_dict=1)
+
+	return data_cash_flow
