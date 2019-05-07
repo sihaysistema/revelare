8a2d4c22f9a670732a6db6f195803c20dd7f9543
monroy95 <m.monroyc22@gmail.com>
2019-05-07 16:41:41 -0600

    Weekly default budgeted cash flow report

--- revelare/revelare/report/budgeted_cash_flow_report/budgeted_cash_flow_report.js
+++ revelare/revelare/report/budgeted_cash_flow_report/budgeted_cash_flow_report.js
@@ -37,7 +37,7 @@ frappe.query_reports["Budgeted Cash Flow Report"] = {
 				{ "value": "Quarterly", "label": __("Quarterly") },
 				{ "value": "Yearly", "label": __("Yearly") }
 			],
-			default: "Yearly",
+			default: "Weekly",
 			reqd: 1
 		}
 	],
