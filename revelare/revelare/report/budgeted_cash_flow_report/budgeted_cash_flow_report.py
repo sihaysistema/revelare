ebcdb3296d645f07838f4df944f41cf663401889
monroy95 <m.monroyc22@gmail.com>
2019-05-07 22:32:02 -0600

    Agregnado comentarios

--- revelare/revelare/report/budgeted_cash_flow_report/budgeted_cash_flow_report.py
+++ revelare/revelare/report/budgeted_cash_flow_report/budgeted_cash_flow_report.py
@@ -15,6 +15,8 @@ from erpnext.accounts.utils import get_fiscal_year
 from six import iteritems
 
 def execute(filters=None):
+	'''Funcion especial ejecutada por frappe para generar la data para el reporte'''
+
 	filters = frappe._dict(filters or {})
 	columns = get_columns(filters)
 
@@ -29,6 +31,7 @@ def execute(filters=None):
 	# Data Ingresos
 	prepare_data = prepare_data_unpaid(filters)
 	data.extend(prepare_data)
+
 	# Agregar total de Ingresos
 	total_unpaid = add_total_row(prepare_data, filters, True)
 	data.extend(total_unpaid)
@@ -40,21 +43,24 @@ def execute(filters=None):
 
 	prepare_data_p = prepare_data_paid(filters)
 	data.extend(prepare_data_p)
+
+	# Agregar total de egresos
 	total_paid = add_total_row(prepare_data_p, filters, False)
 	data.extend(total_paid)
 
-	# with open('dataTest.json', 'w') as f:
-	# 	f.write(str(json.dumps(total_paid, indent=2)))
-
+	# Agregar total, (Ingresos - Egresos)
 	total_cash_flow = add_total_row_report(total_unpaid, total_paid, filters)
 	data.extend(total_cash_flow)
 
+	# Data para generar graficas
 	chart = get_chart_data(columns)
 
 	return columns, data, None, chart
 
 
 def get_columns(filters):
+	'''Genera las columnas necesarias para el reporte'''
+
 	columns = [{
 		"fieldname": "name",
 		"label": _("Party Cash Flow"),
@@ -65,6 +71,7 @@ def get_columns(filters):
 
 	ranges = get_period_date_ranges(filters)
 
+	# Genera las columnas en base al rango de fechas
 	for dummy, end_date in ranges:
 		period = get_period(end_date, filters)
 
@@ -79,35 +86,39 @@ def get_columns(filters):
 
 
 def get_period_date_ranges(filters):
-		from dateutil.relativedelta import relativedelta
-		from_date, to_date = getdate(filters.from_date), getdate(filters.to_date)
+	'''Obtiene el periodo en base al rango de fechas'''
 
-		increment = {
-			"Monthly": 1,
-			"Quarterly": 3,
-			"Half-Yearly": 6,
-			"Yearly": 12
-		}.get(filters.range,1)
+	from dateutil.relativedelta import relativedelta
+	from_date, to_date = getdate(filters.from_date), getdate(filters.to_date)
 
-		periodic_daterange = []
-		for dummy in range(1, 53, increment):
-			if filters.range == "Weekly":
-				period_end_date = from_date + relativedelta(days=6)
-			else:
-				period_end_date = from_date + relativedelta(months=increment, days=-1)
+	increment = {
+		"Monthly": 1,
+		"Quarterly": 3,
+		"Half-Yearly": 6,
+		"Yearly": 12
+	}.get(filters.range,1)
 
-			if period_end_date > to_date:
-				period_end_date = to_date
-			periodic_daterange.append([from_date, period_end_date])
+	periodic_daterange = []
+	for dummy in range(1, 53, increment):
+		if filters.range == "Weekly":
+			period_end_date = from_date + relativedelta(days=6)
+		else:
+			period_end_date = from_date + relativedelta(months=increment, days=-1)
 
-			from_date = period_end_date + relativedelta(days=1)
-			if period_end_date == to_date:
-				break
+		if period_end_date > to_date:
+			period_end_date = to_date
+		periodic_daterange.append([from_date, period_end_date])
 
-		return periodic_daterange
+		from_date = period_end_date + relativedelta(days=1)
+		if period_end_date == to_date:
+			break
+
+	return periodic_daterange
 
 
 def get_period(posting_date, filters):
+	'''retorna el periodo en base al filtro del reporte'''
+
 	months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 	if filters.range == 'Weekly':
@@ -124,12 +135,14 @@ def get_period(posting_date, filters):
 
 
 def prepare_data_unpaid(filters):
+	'''Prepara la data en base a los registros pendientes por cobrar
+		la estructura retornada sera un array con multiples diccionarios
+		donde cada diccionario corresponde a una fila en el reporte
+	'''
+
 	data = []
-	# items = get_reg_unpaid(filters)
-	# sle = get_stock_ledger_entries(filters, items)
-	# item_details = get_item_details(items, sle, filters)
+	# Obtiene los items registrados pendientes por cobrar
 	item_details = get_reg_unpaid(filters)
-	# periodic_data = get_periodic_data(sle, filters)
 	ranges = get_period_date_ranges(filters)
 
 	for item_data in item_details:
@@ -144,7 +157,6 @@ def prepare_data_unpaid(filters):
 			period = get_period(end_date, filters)
 			fecha_registro = conversion_fechas(item_data.posting_date, filters)
 
-			# frappe.msgprint(_(period))
 			if fecha_registro == period:
 				amount = flt(item_data.paid_amount)
 				row[scrub(period)] = amount
@@ -161,10 +173,13 @@ def prepare_data_unpaid(filters):
 
 
 def prepare_data_paid(filters):
+	'''Prepara la data en base a los registros pagados (EGRESOS)
+		la estructura retornada sera un array con multiples diccionarios
+		donde cada diccionario corresponde a una fila en el reporte
+	'''
+
 	data = []
-	# items = get_reg_unpaid(filters)
-	# sle = get_stock_ledger_entries(filters, items)
-	# item_details = get_item_details(items, sle, filters)
+	# Obtiene los registros pagados (gastos)
 	item_details = get_reg_paid(filters)
 	# periodic_data = get_periodic_data(sle, filters)
 	ranges = get_period_date_ranges(filters)
@@ -198,6 +213,8 @@ def prepare_data_paid(filters):
 
 
 def get_chart_data(columns):
+	'''retorna la data necesaria para mostrar las graficas en el reporte'''
+
 	labels = [d.get("label") for d in columns[1:]]
 	chart = {
 		"data": {
@@ -211,6 +228,10 @@ def get_chart_data(columns):
 
 
 def get_reg_unpaid(filters):
+	'''Retorna la data solicitada de la base de datos, especificamente los registros
+		pendientes por cobrar
+	'''
+
 	data_cash_flow = frappe.db.get_values('Budgeted Cash Flow',
 										filters={'company': filters.get("company"), 'status_payment': 'Unpaid'},
 										fieldname=['name', 'party', 'paid_amount', 'posting_date',
@@ -220,6 +241,10 @@ def get_reg_unpaid(filters):
 
 
 def get_reg_paid(filters):
+	'''Retorna la data solicitada de la base de datos, especificamente los registros
+		pagados (EGRESOS)
+	'''
+
 	data_cash_flow = frappe.db.get_values('Budgeted Cash Flow',
 										filters={'company': filters.get("company"), 'status_payment': 'Paid'},
 										fieldname=['name', 'party', 'paid_amount', 'posting_date',
@@ -229,6 +254,8 @@ def get_reg_paid(filters):
 
 
 def conversion_fechas(fecha, filters):
+	'''Funcion customizada para trabajar fechas especialmente semanal TODO: daily'''
+
 	if filters.range == 'Weekly':
 		period = 'Week ' + str(fecha.isocalendar()[1]) + ' ' + str(fecha.year)
 	elif filters.range == 'Monthly':
@@ -244,6 +271,8 @@ def conversion_fechas(fecha, filters):
 
 
 def add_total_row(out, filters, tipo=False):
+	'''Agrega el total de ingresos como egresos'''
+
 	data = []
 
 	if tipo:
@@ -287,6 +316,8 @@ def add_total_row(out, filters, tipo=False):
 
 
 def add_total_row_report(total_in, total_e, filters):
+	'''Total del reporte resta la suma de ingresos con la suma de egresos'''
+
 	data = []
 
 	row_data = frappe._dict({
