6fb4cfa67b4edf754783416f0e6cfe2d9e04d427
monroy95 <m.monroyc22@gmail.com>
2019-06-21 17:32:30 -0600

    Agregando filas

---
+++ revelare/revelare/page/tabular_delivery_not/configuration.js
@@ -0,0 +1,2 @@
+export var rows = 25;
+export var data_template = ['', '', '', '', '', '', '', '', ''];
--- revelare/revelare/page/tabular_delivery_not/tabular_delivery_not.js
+++ revelare/revelare/page/tabular_delivery_not/tabular_delivery_not.js
@@ -1,11 +1,105 @@
-var rows = 25
+var rows = 25;
 var data_content = [];
+var data_template = ['', '', '', '', '', '', '', '', ''];
+const column1 = {
+	name: "Serie",
+	id: 'serie',
+	editable: true,
+	resizable: true,
+	sortable: false,
+	focusable: true,
+	dropdown: false,
+	width: 80
+}
+const column2 = {
+	name: "Numero",
+	id: 'numero',
+	editable: true,
+	resizable: true,
+	sortable: false,
+	focusable: true,
+	dropdown: false,
+	width: 80
+}
+const column3 = {
+	name: "Factura",
+	id: 'serie',
+	editable: true,
+	resizable: true,
+	sortable: false,
+	focusable: true,
+	dropdown: false,
+	width: 80
+}
+const column4 = {
+	name: "Total Factura",
+	id: 'serie',
+	editable: true,
+	resizable: true,
+	sortable: false,
+	focusable: true,
+	dropdown: false,
+	width: 80
+}
+const column5 = {
+	name: "Cliente",
+	id: 'serie',
+	editable: true,
+	resizable: true,
+	sortable: false,
+	focusable: true,
+	dropdown: false,
+	width: 80
+}
+const column6 = {
+	name: "Monto del Vale",
+	id: 'monto_del_vale',
+	editable: true,
+	resizable: true,
+	sortable: false,
+	focusable: true,
+	dropdown: false,
+	width: 80
+}
+const column7 = {
+	name: "Producto",
+	id: 'producto',
+	editable: true,
+	resizable: true,
+	sortable: false,
+	focusable: true,
+	dropdown: false,
+	width: 80
+}
+const column8 = {
+	name: "Precio",
+	id: 'precio',
+	editable: true,
+	resizable: true,
+	sortable: false,
+	focusable: true,
+	dropdown: false,
+	width: 80
+}
+const column9 = {
+	name: "Cantidad",
+	id: 'cantidad',
+	editable: true,
+	resizable: true,
+	sortable: false,
+	focusable: true,
+	dropdown: false,
+	width: 80
+}
+var column_content = ['Serie', 'Numero', 'Factura', 'Total Factura', 'Cliente', 'Monto del Vale', 'Producto', 'Precio', 'Cantidad' ]
+//var column_content = [column1, column2, column3, column4, column5, column6, column7, column8, column9]
 
 for (row = 0; row < rows; row++) {
   // Runs 22 times, with values of step 0 through 4.
-	data_content.push(['', '', '', '', '', '', '', '', '']);
-  	console.log('Se agrego una fila' + row);
+	data_content.push(data_template);
+	//console.log('Se agrego una fila' + row);
 }
+var data = data_content
 
 frappe.pages['tabular-delivery-not'].on_page_load = function (wrapper) {
 	var page = frappe.ui.make_app_page({
@@ -20,46 +114,32 @@ frappe.pages['tabular-delivery-not'].on_page_load = function (wrapper) {
 	// create a container that will render the datatable.
 	const container = '.layout-main-section';
 	
+	function prepare_configuration(){
+	
+	}
+	
 	function load_dependencies() {
 		frappe.require(["/assets/frappe/css/frappe-datatable.css",
 			"/assets/frappe/js/lib/clusterize.min.js",
 			"/assets/frappe/js/lib/Sortable.min.js",
-			"/assets/frappe/js/lib/frappe-datatable.js"], function(){
-					const column1 = {
-						name: 'Serie',
-						id: 'serie',
-						editable: true,
-						resizable: true,
-						sortable: false,
-						focusable: true,
-						dropdown: false,
-						width: 80
-					}
-					const column2 = {
-						name: 'Numero',
-						id: 'serie',
-						editable: true,
-						resizable: true,
-						sortable: false,
-						focusable: true,
-						dropdown: false,
-						width: 80
-					}
+		"/assets/frappe/js/lib/frappe-datatable.js"], function(){
+
 				const options = {
-					columns: [column1, 'Numero', 'Factura', 'Total Factura', 'Cliente', 'Monto del Vale', 'Producto', 'Precio', 'Cantidad' ],
+					columns: column_content,
 					data: data_content
 				}
 				let dt = new DataTable('.layout-main-section', options);
 			});
 	}
-	
+
 	function render_page() {
 
 	}
-	
+
 	frappe.run_serially([
+		//() => prepare_configuration(),
 		() => load_dependencies(),
-		() => render_page()
+		//() => render_page()
 	]);
 	
 };
