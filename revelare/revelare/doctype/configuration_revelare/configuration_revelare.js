14527f2b9ca6a64ef050c2e1bc44a5da88c75d7e
monroy95 <m.monroyc22@gmail.com>
2019-07-02 20:15:36 -0600

    base para creacion de notas de entrega

--- revelare/revelare/doctype/configuration_revelare/configuration_revelare.js
+++ revelare/revelare/doctype/configuration_revelare/configuration_revelare.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Configuration Revelare', {
-	refresh: function(frm) {
-
+	refresh: function (frm) {
+		frm.add_custom_button(__('Test'), function () {
+			frappe.call({
+				method: "revelare.utils_revelare.delivery_note.template_impuestos",
+				callback: function (r) {
+					//console.log(r);
+					//frappe.msgprint(r);
+					//frm.doc.cae = r.message
+					// frm.reload_doc()
+				}
+			})
+		}).addClass("btn-primary");
 	}
 });
--- revelare/utils_revelare/delivery_note.py
+++ revelare/utils_revelare/delivery_note.py
@@ -5,17 +5,61 @@ from frappe import _
 import json
 
 
+def validar_configuracion():
+    '''Permite verificar que exista una configuración validada para revelare,
+       retorna 1 de 3 opciones:
+       1 = Una configuracion valida
+       2 = Hay mas de una configuracion
+       3 = No hay configuraciones
+    '''
+    # verifica que exista un documento validado, docstatus = 1 => validado
+    if frappe.db.exists('Configuration Revelare', {'docstatus': 1}):
+
+        configuracion_valida = frappe.db.get_values('Configuration Revelare',
+                                                   filters={'docstatus': 1},
+                                                   fieldname=['name'], as_dict=1)
+        if (len(configuracion_valida) == 1):
+            return (int(1), str(configuracion_valida[0]['name']))
+
+        elif (len(configuracion_valida) > 1):
+            return (int(2), 'Error 2')
+
+    else:
+        return (int(3), 'Error 3')
+
+
+@frappe.whitelist()
+def template_impuestos():
+    '''Funcion encargada de obtener el template de impuestos a
+       utilizar en la creacion de Delivery Note en base a la
+       Configuracion de revelare
+    '''
+
+    c_valida = validar_configuracion()
+
+    if c_valida[0] == 1:
+        configuracion = frappe.db.get_values('Configuration Revelare',
+                                             filters={'name': c_valida[1]},
+                                             fieldname=['template_impuestos_venta'],
+                                             as_dict=1)
+
+        data_impuestos = frappe.db.get_values('Sales Taxes and Charges',
+                                              filters={'parent': configuracion[0]['template_impuestos_venta']},
+                                              fieldname=['charge_type', 'base_tax_amount',
+                                                         'tax_amount', 'description',
+                                                         'base_tax_amount_after_discount_amount',
+                                                         'base_total', 'included_in_print_rate',
+                                                         'rate', 'account_head', 'cost_center',
+                                                         'tax_amount_after_discount_amount',
+                                                         'total'], as_dict=1)
+
+        return data_impuestos
+
+
 def crear_nota_entrega(documento):
     '''Funcion para crear nota de entrega'''
 
-    delivery_note_tax = [{
-        "charge_type": _('On net total'),
-        "account_head": 'IVA - S',
-        "cost_center": 'Main - S',
-        "description": 'IVA @ 12.0',
-        "included_in_print_rate": 1,
-        "rate": 12
-    }]
+    delivery_note_tax = template_impuestos()
 
     delivery_note_items = [{
         "item_code": 'GAS-001',
@@ -41,3 +85,4 @@ def crear_nota_entrega(documento):
     DN_created = delivery_note.insert(ignore_permissions=True)
 
     return 'OK'
+
