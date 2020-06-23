1b02d548afd78059efbfe5571ccc54164864774b
alain <fogliasana@gmail.com>
2020-06-23 05:47:08 +0000

    basic inclusion of plotly

--- revelare/hooks.py
+++ revelare/hooks.py
@@ -17,8 +17,32 @@ fixtures = ["Custom Field"]
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/revelare/css/revelare.css"
-app_include_js = "/assets/revelare/js/revelar.js"
+# app_include_js = "/assets/revelare/js/revelar.min.js"
 
+app_include_js = [
+    "/assets/revelare/js/revelar.min.js",
+    "/assets/revelare/js/plotly_start_tool.min.js",
+    "/assets/revelare/node_modules/plotly.js/dist/plotly.min.js",
+    "/assets/revelare/node_modules/plotly.js/dist/plotly-gl3d.min.js",
+    "/assets/revelare/node_modules/plotly.js/dist/plotly-gl2d.min.js",
+    "/assets/revelare/node_modules/plotly.js/dist/plotly-geo.min.js",
+    "/assets/revelare/node_modules/plotly.js/dist/plotly-basic.min.js",
+    "/assets/revelare/node_modules/plotly.js/dist/plotly-cartesian.min.js",
+    "/assets/revelare/node_modules/plotly.js/dist/plotly-finance.min.js"
+]
+
+'''
+app_include_js = [
+    "/assets/revelare/js/revelar.min.js",
+    "/assets/node_modules/plotly.js/plotly.min.js",
+    "/assets/node_modules/plotly.js/plotly-gl3d.min.js",
+    "/assets/node_modules/plotly.js/plotly-gl2d.min.js",
+    "/assets/node_modules/plotly.js/plotly-geo.min.js",
+    "/assets/node_modules/plotly.js/plotly-basic.min.js",
+    "/assets/node_modules/plotly.js/plotly-cartesian.min.js",
+    "/assets/node_modules/plotly.js/plotly-finance.min.js"
+]
+'''
 # include js, css files in header of web template
 # web_include_css = "/assets/revelare/css/revelare.css"
 # web_include_js = "/assets/revelare/js/revelare.js"
---
+++ revelare/public/js/plotly_start_tool.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2018, Si Hay Sistema (SHS) and contributors
+// For license information, please see license.txt
+
+console.log('Hello from plotly startup tool');
+frappe.msgprint('Plotly-startup cargado exitosamente');
+//frappe.provide('frappe.revelare');
+
+// works, with some errors.
+import Plotly from '../node_modules/plotly.js/dist/plotly.js'
+
+/*
+frappe.require('/assets/revelare/js/plotly_start_tool.min.js', () => {
+            // Awesome code here
+    });
+*/
