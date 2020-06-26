424e919b3b6cdee75a23d6bea2bc480479f23a6f
alain <fogliasana@gmail.com>
2020-06-26 21:22:43 +0000

    adding boiler for graph

---
+++ revelare/public/vue/PlotlyGraph.vue
@@ -0,0 +1,17 @@
+<template>
+  <button v-on:click="count++">SHS Vue Custom Button — {{ count }}</button>
+  <!-- Seems like a VUE component is an HTML page, trying a comment out-->
+</template>
+<!-- Seems like a VUE component is an HTML page, trying a comment out-->
+<script>
+// Works great!
+console.log("VUE Component Button Counter Loaded");
+export default {
+  props: ["count"],
+  data: function() {
+    return {
+      count: 0
+    };
+  }
+};
+</script>
