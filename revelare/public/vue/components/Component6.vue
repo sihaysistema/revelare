faab7c82fc5a570f0f0751f4b369b2f5f9649e67
alain <fogliasana@gmail.com>
2020-07-02 00:13:29 +0000

    michelle's button

---
+++ revelare/public/vue/components/Component6.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+  <p>Random Number = {{ randomNumber }}</p>
+  <button v-on:click="() => myFunction()">Click Me 2</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["randomNumber"],
+    data: {},
+    methods: {
+      myFunction: function (event) {
+            var randomNumber = 0;
+            this.randomNumber = Math.floor(Math.random()*100); //multiply to generate random number between 0, 100
+            // console.log(randomNumber);
+            return randomNumber;
+        }
+      }
+    };
+</script>
+
