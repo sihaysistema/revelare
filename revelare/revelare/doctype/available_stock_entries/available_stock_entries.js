6acce984fb6f60d603d6e36b55d77ed3d56e0d4d
Tropicalrambler <alainber@gmail.com>
2020-10-07 04:19:04 +0000

    modifications

--- revelare/revelare/doctype/available_stock_entries/available_stock_entries.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020, SHS and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Available Stock Entries', {
-	// refresh: function(frm) {
-
-	// }
-});
