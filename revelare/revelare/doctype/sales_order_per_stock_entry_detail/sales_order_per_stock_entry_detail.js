71706164e995ba475e3af2f0030501d4ebd19e3e
Tropicalrambler <alainber@gmail.com>
2020-10-07 04:09:45 +0000

    #57 Sales Order per Stock Entry Detail

---
+++ revelare/revelare/doctype/sales_order_per_stock_entry_detail/sales_order_per_stock_entry_detail.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, SHS and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sales Order per Stock Entry Detail', {
+	// refresh: function(frm) {
+
+	// }
+});
