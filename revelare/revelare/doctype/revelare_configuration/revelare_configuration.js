f5b1581494a51057c9fe0e3f852b3217e5eb41e6
monroy95 <m.monroyc22@gmail.com>
2020-10-07 16:38:54 +0000

    #56 doctype configuration

---
+++ revelare/revelare/doctype/revelare_configuration/revelare_configuration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, SHS and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Revelare Configuration', {
+	// refresh: function(frm) {
+
+	// }
+});
