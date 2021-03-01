df4ceef55754eb0623b45fa24546e92b1996a547
ronaldo ramirez <perencen17@gmail.com>
2021-03-01 16:45:44 +0000

    creating free cash flow and GEV report #85

---
+++ revelare/revelare/report/generated_economic_value_report/generated_economic_value_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, SHS and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Generated Economic Value Report"] = {
+	"filters": [
+
+	]
+};
