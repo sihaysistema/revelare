f440f16557253c1216d06dcb85955f92251feb11
ronaldo ramirez <perencen17@gmail.com>
2021-03-11 18:51:18 +0000

    Changing component column width #82

--- revelare/revelare/report/direct_cash_flow_report/direct_cash_flow_report.py
+++ revelare/revelare/report/direct_cash_flow_report/direct_cash_flow_report.py
@@ -26,7 +26,7 @@ def get_columns(filters):
             "label": _("Components"),
             "fieldname": "name",
             "fieldtype": "Data",
-            "width": 200
+            "width": 365
         }
     ]
 
