cd027bcb7bfb81c5cee218e8fa659d62c87e375c
ronaldo ramirez <perencen17@gmail.com>
2021-10-14 21:21:13 -0600

    #153 address formatting in the address field and in the contact field

--- revelare/revelare/doctype/errand_trip/errand_trip.js
+++ revelare/revelare/doctype/errand_trip/errand_trip.js
@@ -2,9 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Errand Trip', {
-  refresh: function (frm) {
+  /*refresh: function (frm) {
     frm.get_field("get_data").$input.addClass("btn btn-primary");
-  },
+  },*/
   get_data: function (frm) {
     // console.log('Vamos a obtener la data');
     frappe.call({
@@ -44,6 +44,117 @@ frappe.ui.form.on('Errand Trip', {
     });
   },
 });
+
+// Agregando direccion al campo de detalle y contacto
+frappe.ui.form.on("Errand Trip Stop", {
+  address: function (frm, cdt, cdn) {
+    let row = frappe.get_doc(cdt, cdn);
+
+    frappe.call({
+      method: 'revelare.revelare.doctype.errand_trip.errand_trip.fetch_address',
+      args: {
+        address: row.address || ''
+      },
+      callback: function (r) {
+        //console.log(r.message);
+        //console.log(typeof(r.message));
+        let address_detail = r.message.string_address;
+        let latitude = r.message.latitude;
+        let longitude = r.message.longitude;
+
+        row.address_details = address_detail;
+        row.lat = latitude;
+        row.lng = longitude;
+        cur_frm.refresh_field('errand_trip_stop');
+        frm.refresh();
+      }
+    });
+  },
+  contact: function (frm, cdt, cdn) {
+
+    let row = frappe.get_doc(cdt, cdn);
+    frappe.call({
+      method: 'revelare.revelare.doctype.errand_trip.errand_trip.fetch_contact',
+      args: {
+        contact: row.contact || ''
+      },
+      callback: function (r) {
+        // console.log(typeof(r.message));
+        let contact_detail = r.message;
+
+        row.contact_details = contact_detail;
+        cur_frm.refresh_field('errand_trip_stop');
+        frm.refresh();
+      }
+    });
+  }
+});
+
+
+// frappe.ui.form.on('Errand Trip Stop', {
+
+//   address: function (frm, cdt, cdn) {
+//     let row = frappe.get_doc(cdt, cdn);
+
+//     row.address_detail = 'Hola Estamos intentando \nagregar texto \n\tformateado';
+//     cur_frm.refresh_field('errand_trip_stop');
+//     frm.refresh();
+
+//     frappe.call({
+//       method: 'revelare.revelare.doctype.errand_trip.errand_trip.fetch_address',
+//       args: {
+//         address: row.address || ''
+//       },
+//       callback: function (r) {
+//         console.log(r.message);
+//       }
+//     });
+
+
+//     // console.log(cdn,'\n\n',cdt,'\n\nEvento Address');
+//     let check = 0;
+//     // Recorre los elementos de la tabla hija ubicandose exactamente en la fila en que
+//     // estemos trabajando, esto para obtener datos exactos.
+//     frm.doc.uoms.forEach((uom_row_i, index_i) => {
+//       if (uom_row_i.name == cdn) {
+//         (uom_row_i.revelare_default_uom_sales_analytics_2) ? frm.enable_save() : console.log('unckecked');
+//         // FORMA 2! RECOMENDADA ES5 > :)
+//         // Unicamente va permitir que exista un elemento seleccionado
+//         frm.doc.uoms.forEach((uom_row, indice) => {
+//           if (uom_row.revelare_default_uom_sales_analytics_2) {
+//             check++;
+//             if (check > 1) {
+//               frappe.msgprint('Solamente debe haber un elemento seleccionado');
+//               frm.doc.uoms[index_i].revelare_default_uom_sales_analytics_2 = 0;
+//               refresh_field('uoms');
+//               // console.log(uom_row_i.revelare_default_uom_sales_analytics_2)
+//             }
+//           }
+//         });
+//       }
+//     });
+//   },
+//   contact: function (frm, cdt, cdn) {
+
+//     let row = frappe.get_doc(cdt, cdn);
+//     frappe.call({
+//       method: 'revelare.revelare.doctype.errand_trip.errand_trip.fetch_contact',
+//       args: {
+//         contact: row.contact || ''
+//       },
+//       callback: function (r) {
+//         console.log(r.message);
+
+//         row.contact_detail = `Hola Estamos intentando \nagregar texto \n\tformateado`;
+//         cur_frm.refresh_field('errand_trip_stop');
+//         frm.refresh();
+//         frm.save();
+//       }
+//     });
+//   }
+// });
+
+
 
 // frappe.listview_settings['Errand Trip'] = {
 //   //  add_fields: ["title", "start_date", "end_date"],
