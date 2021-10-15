ed35144c52f5507b06a6691474a13fef3d443239
monroy95 <m.monroyc22@gmail.com>
2021-10-15 20:52:32 +0000

    Fixtures campos waze

--- revelare/public/js/revelar.js
+++ revelare/public/js/revelar.js
@@ -293,59 +293,32 @@ function open_one_tab(name, type) {
 
 frappe.ui.form.on('Address', {
   onload_post_render: function (frm) {
-    // load_dependencies_mapa()
-    console.log("Si jalo")
-    var map = L.map('map').setView([14.552792466859572, -90.45385210407252], 15);
+    let latitude = frm.doc.shs_latitude;
+    let longitude = frm.doc.shs_longitude;
+    let zoom = 15;
 
+    // Crea una nueva instancia de mapa, con una ubicacion default
+    var map = L.map('map').setView([latitude, longitude], zoom);
+
+    // El proveedor de mapas es openstreetmap, si se require mas precision hay que pagar por API KEY
+    // como Google Maps
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
+    // Se agregan los controles de busqueda y zoom al mapa
     var searchControl = L.esri.Geocoding.geosearch().addTo(map);
 
+    // Los resultados se agregan como capa al mapa (frente)
     var results = L.layerGroup().addTo(map);
 
+    // Cuando se realiza una busqueda de dirección, en el icono de lupa
     searchControl.on('results', function (data) {
-      console.log("Buscando")
-      results.clearLayers();
+      results.clearLayers();  // Se limpia la capa para renderizar los nuevos resultados
       for (var i = data.results.length - 1; i >= 0; i--) {
         results.addLayer(L.marker(data.results[i].latlng));
         console.log("Resultado", data.results[i].latlng)
       }
     });
   },
-  refresh: function (frm) {
-
-    // load_dependencies_mapa()
-
-  }
 });
-
-function load_dependencies_mapa() {
-  frappe.require([
-    "/assets/revelare/css/leaflet.css",
-    "/assets/revelare/css/esri-leaflet-geocoder.css",
-    "/assets/revelare/js/leafletmap/leaflet.js",
-    "/assets/revelare/js/leafletmap/esri-leaflet.js",
-    "/assets/revelare/js/leafletmap/esri-leaflet-geocoder.js"
-  ], () => {
-    // console.log("Si jalo")
-    // var map = L.map('map').setView([14.552792466859572, -90.45385210407252], 20);
-
-    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //   attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-    // }).addTo(map);
-
-    // var searchControl = L.esri.Geocoding.geosearch().addTo(map);
-
-    // var results = L.layerGroup().addTo(map);
-
-    // searchControl.on('results', function (data) {
-    //   results.clearLayers();
-    //   for (var i = data.results.length - 1; i >= 0; i--) {
-    //     results.addLayer(L.marker(data.results[i].latlng));
-    //     console.log("Resultado", data.results[i].latlng)
-    //   }
-    // });
-  });
-}
