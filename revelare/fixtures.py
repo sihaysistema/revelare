c6101a62dd42054cb3ffcc98e70d032436bafabc
monroy95 <m.monroyc22@gmail.com>
2021-10-18 22:26:09 +0000

    Regresando fixtures de direct_cash_flow_component y excluyendolo en la exportacion de fixtures.py'

--- revelare/fixtures.py
+++ revelare/fixtures.py
@@ -112,7 +112,10 @@ def fill_fixtures():
     # NEW FUNCTIONALITY FOR EXPORTING
     fixtures_fillup.append(custom_field)
     fixtures_fillup.append(translation)
-    fixtures_fillup.append(dcf_component)
+
+    # 18/10/2021 Descomentar para fixtures Direct Cash Flow Component
+    # fixtures_fillup.append(dcf_component)
+
     # fixtures_fillup.append(tax_category)
     # fixtures_fillup.append(id_doctype)
 
