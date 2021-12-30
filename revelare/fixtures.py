48a07d32cd59c08b3a3affc72729ebc3106490e9
ronaldo ramirez <perencen17@gmail.com>
2021-12-30 09:44:26 -0600

    #185 New direct cash flow categories

--- revelare/fixtures.py
+++ revelare/fixtures.py
@@ -79,7 +79,68 @@ def fill_fixtures():
                     "Sales Estimates",
                     "Target Warehouse",
                     "Allocated",
-                    "Outstanding"
+                    "Outstanding",
+                    "Cash Effect",
+                    "1 - Company",
+                    "A - Operations",
+                    "A.1 - Cash Payment for Operating Expenses",
+                    "A.1.1 - Cash Payments to Employees",
+                    "A.1.1.1 - Standard Payroll Payments",
+                    "A.1.1.2 - Bonus Payroll Payments",
+                    "A.1.2 - Cash Payments for Severance or Retirement",
+                    "A.1.2.1 - Payments for Retirement",
+                    "A.1.2.2 - Payments for Severance",
+                    "A.1.3 - Cash Payments for Taxes",
+                    "A.1.3.1 - Payments for Sales Taxes",
+                    "A.1.3.2 - Payments for Payroll Taxes",
+                    "A.1.3.3 - Payments for Property Taxes",
+                    "A.1.3.4 - Payments for Other Taxes",
+                    "A.1.3.5 - Payments for Income Taxes",
+                    "A.1.4 - Cash Payments to Suppliers",
+                    "A.1.4.1 - Merchandise or Raw Material payments to Suppliers",
+                    "A.1.4.2 - Operating Expense payments to Suppliers",
+                    "A.1.5 - Cash Receipts from Customers",
+                    "A.1.5.1 - Receipts from sales of goods and services",
+                    "A.1.6 - Interest and Dividends Received",
+                    "A.1.6.2 - Coupons Received from Bonds",
+                    "A.1.6.3 - Interest Received from Investment",
+                    "B - Investment",
+                    "B.1 - Board Compensation Cash Flows",
+                    "B.1.1 - Board Compensation from Participation Interest",
+                    "B.2 - Cash Flows of Taxes Related to Investing Activities",
+                    "B.2.1 - Cash Payments for Taxes Related to Investing",
+                    "B.3 - Fixed Asset Cash Flows",
+                    "B.3.1 - Purchases of fixed assets (Property, Plant & Equipment)",
+                    "B.3.2 - Sale of fixed assets (Property, Plant & Equipment)",
+                    "B.3.3 - Net Investment (CAPEX)",
+                    "B.4 - Loans Given to Others",
+                    "B.4.1 - Money Lent to Individuals",
+                    "B.4.2 - Money Lent to Institutions",
+                    "B.5 - Long-term Securities Cash Flows",
+                    "B.5.1 - Investment in long-term securities (stocks or bonds)",
+                    "B.5.2 - Sale of long-term securities (stocks or bonds)",
+                    "C - Financing",
+                    "C.1 - Debt Service",
+                    "C.1.1 - Coupon Payments",
+                    "C.1.1.1 - Interest Expenses",
+                    "C.1.1.2 - Coupon or Interest Receipts",
+                    "C.1.1.3 - Coupon Amortization",
+                    "C.1.2 - Financial Insitutions Flows",
+                    "C.1.2.1 - Financial Insitutions Transaction Fees",
+                    "C.1.3 - Loans Obtained from Others",
+                    "C.1.3.1 - Money Loaned from Individuals",
+                    "C.1.3.2 - Money Loaned from Institutions",
+                    "C.1.3.3 - Receipts from Issue of Bonds",
+                    "C.1.3.4 - Debt Repayment for Individuals",
+                    "C.1.3.5 - Debt Repayment for Institutions",
+                    "C.1.3.6 - Payments for Bond Cancellations",
+                    "C.1.4 - Shares (Stock)",
+                    "C.1.4.1 - Dividend Payments",
+                    "C.1.4.2 - New Stock Issue",
+                    "C.1.4.3 - Capital Increase",
+                    "C.1.4.4 - Stock Repurchases",
+                    "C.1.5 - Other Financial Flows",
+                    "","","","",""
                 ]
             ]
         ]
@@ -114,9 +175,7 @@ def fill_fixtures():
     # NEW FUNCTIONALITY FOR EXPORTING
     fixtures_fillup.append(custom_field)
     fixtures_fillup.append(translation)
-
-    # 18/10/2021 Descomentar para fixtures Direct Cash Flow Component
-    # fixtures_fillup.append(dcf_component)
+    fixtures_fillup.append(dcf_component)
 
     # fixtures_fillup.append(tax_category)
     # fixtures_fillup.append(id_doctype)
